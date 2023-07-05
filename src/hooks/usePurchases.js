7457ad479561791382119d3b0a4f66becb0b0177
waltercp <walter_cp_96@hotmail.com>
2023-07-05 00:46:04 -0600

    purchases

--- src/hooks/usePurchases.js
+++ src/hooks/usePurchases.js
@@ -5,7 +5,8 @@ import { useState } from "react"
 import axios from "axios"
 
 
-const url = 'https://e-commerce-api-v2.academlo.tech/api/v1/purchases' 
+const URL_BASE = import.meta.env.VITE_REACT_APP_URL
+const url = `${URL_BASE}/purchase`
 
 const usePurchases =() => {
 
